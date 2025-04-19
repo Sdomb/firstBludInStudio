f0733d3da15b9fdc104cfaa95cf2d90118dc8f1a
dombrovskiysemen <dombrovskiysemen@yandex.ru>
2025-04-19 22:09:56 +0300

    5-е задание

--- app/src/main/java/com/example/firstbludinstudio/MainActivity.kt
+++ app/src/main/java/com/example/firstbludinstudio/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.firstbludinstudio
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,11 +17,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Timber.plant(Timber.DebugTree())
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.four)) { v, insets ->
+        setContentView(R.layout.activity_five)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.five)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textInput = findViewById<EditText>(R.id.input)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val t = textInput.text.toString()
+            Log.d("MY MESSAGE", "Ddtltyysq ntrcn $t")
+            Timber.d("ТИШИНА! ТИМБЕР ГОВОРИТ! $t")
+
         }
     }
 }
