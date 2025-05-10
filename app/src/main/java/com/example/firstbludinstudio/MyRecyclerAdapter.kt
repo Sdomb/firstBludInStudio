210a23d4f124a766be9ff3adb61ae0d49ac7b620
dombrovskiysemen <dombrovskiysemen@yandex.ru>
2025-05-10 19:38:52 +0300

    10-е задание
    Создал экран с "плитками" зверей

--- app/src/main/java/com/example/firstbludinstudio/MyRecyclerAdapter.kt
+++ app/src/main/java/com/example/firstbludinstudio/MyRecyclerAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.firstbludinstudio
 
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
